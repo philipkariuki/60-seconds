54ef74876dcbc45ceeaad148ca80f51456221535
philipkariuki <kariukiphil@gmail.com>
2019-07-01 11:51:26 +0300

    updates manage.py

--- manage.py
+++ manage.py
@@ -21,7 +21,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User)
+    return dict(app = app,db = db,User = User, Comments=Comments, Pitches=Pitches)
 
 
 if __name__ == '__main__':
