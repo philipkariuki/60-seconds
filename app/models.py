694bc1be2acce1483a8a27eab2f64c86b79a648a
philipkariuki <kariukiphil@gmail.com>
2019-07-01 11:20:47 +0300

    in models.py creates class Categories under database table name categories

--- app/models.py
+++ app/models.py
@@ -70,6 +70,26 @@ class Pitches(db.Model):
 
 
 
+class Categories(db.Model):
+    __tablename__ = 'categories'
+
+    
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(255))
+    description = db.Column(db.String(255))
+
+    
+    def save_categories(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_categories(cls):
+        categories = Category.query.all()
+        return categories
+
+
+
 
 
 
